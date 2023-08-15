aacf9dfef9415a48aaf85683fe64a86dad2f8ed0
SantiagoRiveros <santiagoriverosjasis@gmail.com>
2023-08-15 10:38:31 -0300

    commit con la explicacion

---
+++ EXPLICACION.js
@@ -0,0 +1,11 @@
+/* Comandos basicos de git
+
+-git clone {url del repositorio} : es la forma de clonar en mi pc, el repositorio que esta en github
+
+Pasos a seguir para subir codigo nuevo a git:
+-git add . : Esto es para agregar los cambios efectuados al commit
+-git commit -m "aca va un comentario" : Es para armar el commit
+-git push : Es para enviar el commit
+
+
+*/
